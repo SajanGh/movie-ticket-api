54f55b30b31dc14ac1f4fdef57a7bfaa37a273a5
Saugat Ghimire <saugat77ghimire@gmail.com>
2023-12-07 20:50:31 +0545

    fix:cors,config

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import routes from "./routes";
 import { Application } from "express-serve-static-core";
 import cors from "cors";
 import "./@config/db.config";
+import { corsUrl } from "./@config/config";
 
 class Main {
   public app: express.Application;
@@ -16,7 +17,9 @@ class Main {
     this.app.use(routes);
   }
   private config(app: Application): void {
-    this.app.use(cors());
+    this.app.use(
+      cors({ origin: corsUrl, optionsSuccessStatus: 200, credentials: true })
+    );
     //support application/json type post data
     this.app.use(bodyParser.json());
     // support application/x-www-form-urlencode post false
